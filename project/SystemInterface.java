2695fe581b630572b9a1f21c3d9e606b3142dfe2
Eliascm17 <elias.cmoreno17@gmail.com>
2018-11-25 21:52:40 -0600

    Transfer and Withdraw
    fixed so that you cannot transfer or withdraw more that you already have

--- project/SystemInterface.java
+++ project/SystemInterface.java
@@ -12,52 +12,71 @@ public class SystemInterface extends AccountFunctions {
         
         System.out.println("Enter deposit amount : ");
         double deposit_amount = input.nextDouble();
-        
-        double new_balance = (a.getBalance() + deposit_amount);
-        
-        a.setBalance(new_balance);
-        last_deposit = deposit_amount;
-        
-        viewBalance(a);
+        if(deposit_amount>0) {
+        	
+	        double new_balance = (a.getBalance() + deposit_amount);
+	        a.setBalance(new_balance);
+	        last_deposit = deposit_amount;
+	        viewBalance(a);
+	        
+        }
+        else {
+        	System.out.println("Please Withdraw Instead");
+        }
         
     }
     
     public void withdraw() {
+    	
+    	Scanner input = new Scanner(System.in);
         
         System.out.println("Enter withdraw amount : ");
         double withdrawal_amount = input.nextDouble();
-        
-        double new_balance = (account.getBalance() - withdrawal_amount);
-        
-        account.setBalance(new_balance);
-        
-        viewBalance(account);
+        if(account.getBalance()>withdrawal_amount) {
+        	
+	        double new_balance = (account.getBalance() - withdrawal_amount);
+	        account.setBalance(new_balance);
+	        viewBalance(account);
+        }
+        else{
+        	System.out.println("Insufficient Funds");
+        }
+        input.close();
         
     }
     
     // TRANSFER FUNDS
     // Still needs to check if funds are available, etc.
     public void transfer() {
+    	
+    	Scanner input = new Scanner(System.in);
         
         System.out.println("Enter destination account");
         String dest= input.nextLine();
         
         Account destination_account = searchAccount(dest);
         
+        while(destination_account==null){transfer();}
+        
         System.out.println("Enter transfer amount : ");
         double transfer= input.nextDouble();
         
         double origin_balance = account.getBalance();
         double destination_balance = destination_account.getBalance();
-        double new_origin_balance = (origin_balance - transfer);
-        double new_destination_balance = (destination_balance + transfer);
         
-        account.setBalance(new_origin_balance);
-        destination_account.setBalance(new_destination_balance);
+        if(origin_balance>=transfer){
         
-        //viewBalance();
-        System.out.println("Destination account balance : " + destination_account.getBalance());
-        
+	        double new_origin_balance = (origin_balance - transfer);
+	        double new_destination_balance = (destination_balance + transfer);
+	        
+	        account.setBalance(new_origin_balance);
+	        destination_account.setBalance(new_destination_balance);
+	        
+	        viewBalance(account);
+	        System.out.println("Destination account balance : " + destination_account.getBalance());
+        }
+	       
+        input.close();
     }
     
     public void viewBalance(Account a) {
